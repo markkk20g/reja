93b65797618dc2e0d68b4744b1dc89cf2e7514f3
markkk20g <abdulmalik.1122@mail.ru>
2025-12-24 16:34:39 +0900

    fix: second part of class

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 console.log('Web Serverni boshlash');
 const express = require('express');
+const res = require("express/lib/response");
 const app = express();
 const http = require('http');
 
@@ -16,12 +17,20 @@ app.set("view engine", "ejs");
 
 
 // 4: Routing code
-app.get("/hello", function(req, res) {
-    res.end(`<h1>Hello World</h1>`);
+// app.get("/hello", function(req, res) {
+//     res.end(`<h1>Hello World</h1>`);
+// });
+
+// app.get("/gift", function(req, res) {
+//     res.end(`<h1>Siz sovgalar bolimidasiz!</h1>`);
+// });
+app.post("/create-item", (req, res) => {
+    console.log(req.body);
+    res.json({test: "success"});
 });
 
-app.get("/gift", function(req, res) {
-    res.end(`<h1>Siz sovgalar bolimidasiz!</h1>`);
+app.get("/", function(req, res) {
+    res.render("harid");
 });
 
 const server = http.createServer(app);
